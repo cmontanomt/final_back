30ec19ac1992950d8960c1edb1058ac0da0b2ff6
cmontanounab <cesar.montano@o365.unab.edu.co>
2022-12-21 08:32:46 -0500

    modify config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 console.log("entorno: "+process.env.PORT_SERVER);
-
-
-
-
+console.log("entorno: "+process.env.DB_USER);
+console.log("entorno: "+process.env.DB_DATABASE);
+console.log("entorno: "+process.env.DB_HOST);
